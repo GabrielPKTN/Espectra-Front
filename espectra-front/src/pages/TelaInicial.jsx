74806fc7a0482ba0d1eac59dba61d4be508e5fe7
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-04-29 08:38:14 -0300

    instalei a versão mais recente do node no mac pois o vite estava exigindo uma versão superior a 20.19 e a do mac era 20.17. Finalizei o layout do header da pagina para o mobile, levando em conta que o design está pensado como mobile first

--- espectra-front/src/pages/TelaInicial.jsx
+++ espectra-front/src/pages/TelaInicial.jsx
@@ -1,25 +1,33 @@
-import logo_v1 from "../assets/logotipos/logo_v1.png"
+import logo_v1 from "../assets/logotipos/logo_v1.png";
 
-function TelaInicial(){
-return(
+function TelaInicial() {
+  return (
+    // div que gurdar tudo na tela.
     <div className="w-screen h-screen">
-        <div className="flex justify-between items-center">
+      {/* header */}
+      <div className="flex justify-between items-center p-1">
+        <img src={logo_v1} alt="logotipo espectra" className="p-4" />
 
-            <img src={logo_v1}
-                alt="logotipo espectra"
-                className="p-4"
-                />
+        {/* NECESSÁRIO COLOCAR OS LINKS DEPOIS */}
+        <nav>
+          <ul className="flex gap-8 p-5 font-sans font-semibold text-lg text-black">
+            <li>
+              <a href="/contato">Contato</a>
+            </li>
+            <li>
+              <a href="/sobre">Sobre</a>
+            </li>
+            <li className="hover:bg-[#3277CF] hover:text-white px-2 rounded-2xl transition-colors duration-200">
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-            <ul className="flex gap-4 p-6 font-sans font-normal">
-                <li>Contato</li>
-                <li>Sobre</li>
-                <li>Login</li>
-            </ul>
-        </div>
+      {/* main */}
+      <div></div>
     </div>
-    
-)
+  );
 }
 
 export default TelaInicial;
-
