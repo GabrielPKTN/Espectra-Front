6328319a665c1e6062373b607ed92b0f61254ba5
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-04-30 11:10:50 -0300

    feat: foto foi adicionada ao sistema. Necessário ajustar para que ela não cresça além do tamanho necessário

--- espectra-front/src/pages/TelaInicial.jsx
+++ espectra-front/src/pages/TelaInicial.jsx
@@ -1,4 +1,5 @@
-import logo_v1 from "../assets/logotipos/logo_v1.png";
+import logo from "../assets/logotipos/logo.png";
+import photo_main from "../assets/general_photos/photo_main.png";
 import NavItem from "../components/NavItem";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import Button from "../components/Button";
@@ -8,10 +9,10 @@ function TelaInicial() {
 
   return (
     // div que gurdar tudo na tela.
-    <div className="w-screen h-screen">
+    <div className="w-screen h-full flex flex-col">
       {/* header */}
       <div className="flex justify-between items-center p-1">
-        <img src={logo_v1} alt="logotipo espectra" className="p-4" />
+        <img src={logo} alt="logotipo espectra" className="p-4 w-32" />
 
         {/* NECESSÁRIO COLOCAR OS LINKS DEPOIS */}
         <nav>
@@ -24,21 +25,29 @@ function TelaInicial() {
       </div>
 
       {/* main */}
-      <div className="my-14 flex flex-col items-center">
-        {/* div do texto que existe no header */}
-        <div className="flex flex-col m-8">
-          <p className="imprima-regular text-2xl">BEM VINDO AO</p>
-          <h1 className="inclusive-sans font-bold text-6xl dark-blue">
-            ESPECTRA
-          </h1>
-          <span className="inclusive-sans text-xl font-light">
-            Acompanhe, registre e celebre cada passo do desenvolvimento.
-          </span>
-        </div>
+      <div className="flex flex-col items-center justify-between">
+        <div className="my-14 flex flex-col items-center">
+          {/* div do texto que existe no header */}
+          <div className="flex flex-col m-8">
+            <p className="imprima-regular text-2xl">BEM VINDO AO</p>
+            <h1 className="inclusive-sans font-bold text-6xl dark-blue">
+              ESPECTRA
+            </h1>
+            <span className="inclusive-sans text-xl font-light">
+              Acompanhe, registre e celebre cada passo do desenvolvimento.
+            </span>
+          </div>
 
-        <Button variantClick="basicClick" onClick={() => navigate("/cadastro")}>
-          Comece a usar
-        </Button>
+          <Button
+            variantClick="basicClick"
+            onClick={() => navigate("/cadastro")}
+          >
+            Comece a usar
+          </Button>
+        </div>
+        <div className="grow">
+          <img src={photo_main} alt="imagem-prof" className="object-contain" />
+        </div>
       </div>
     </div>
   );
