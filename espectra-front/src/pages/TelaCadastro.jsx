a7c67769961e5d09e5a9a14ee88036ed43c00924
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-02 11:01:50 -0300

    feat: criei a nova branch e iniciei a tela que devera ser de cadastro

--- espectra-front/src/pages/TelaCadastro.jsx
+++ espectra-front/src/pages/TelaCadastro.jsx
@@ -1,5 +1,17 @@
+import logotipo from "../assets/logotipos/logotipo.svg";
+
 function TelaCadastro() {
-  return <h1>TELA PARA CADASTRAR USUÁRIOS</h1>;
+  return (
+    // div que guarda tudo que estiver da tela
+    <div className="flex flex-col bg-primary-color w-screen h-screen">
+      <div className="flex justify-center items-center p-12">
+        <img src={logotipo} alt="logotipo espectra" className="w-auto h-32"/>
+      </div>
+      
+
+
+    </div>
+  )
 }
 
 export default TelaCadastro;
