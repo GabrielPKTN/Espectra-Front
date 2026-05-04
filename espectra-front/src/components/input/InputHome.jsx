96b6ab20343c4db5ea2312b072efd9353e6cc365
GABRIEL LACERDA CORREIA <25132902@dsn127m1242316.cfp127aluno.local>
2026-05-04 08:23:01 -0300

    feat: Adiciona componente input da tela home

---
+++ espectra-front/src/components/input/InputHome.jsx
@@ -0,0 +1,11 @@
+function InputHome() {
+
+
+    return <div className="relative">
+        <input type="text" className="border border-[--primary-color] pl-7"/>
+        <Search className="absolute left-2.5 top-1/2 translate-y-[-50%] w-4 h-4"/>
+    </div>
+
+}
+
+export default InputHome
