257153a3ad24dfa0f8d8e46be76f465198bf28bf
GABRIEL LACERDA CORREIA <25132902@dsn127m1242316.cfp127aluno.local>
2026-05-04 09:28:15 -0300

    test: Adiciona container da foto do usuário

---
+++ espectra-front/src/components/ContainerUserPhoto.jsx
@@ -0,0 +1,9 @@
+function ContainerUserPhoto() {
+
+    return <div className="border border-[--bg-primary-color] rounded-[50%] w-16 h-16">
+        <img src="https://source.unsplash.com/random" alt="user photo" />
+    </div>
+
+}
+
+export default ContainerUserPhoto
---
+++ espectra-front/src/pages/TelaHome.jsx
@@ -0,0 +1,22 @@
+import ContainerInputHome from "../components/input/InputHome.jsx"
+import ContainerUserPhoto from "../components/ContainerUserPhoto"
+
+function TelaHome() {
+
+    return <div>
+
+        <header className="w-screen h-screen">
+            <img src="../assets/logotipos" alt="icone da aplicação" />
+            <div className="w-full h-full">
+                <ContainerUserPhoto/>
+            </div>
+        </header>
+        <main>
+
+        </main>
+
+    </div>
+
+}
+
+export default TelaHome
