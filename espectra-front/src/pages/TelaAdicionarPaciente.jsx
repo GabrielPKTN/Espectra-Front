bc07133d8ed2af7240ff653977c8ffe8ea678469
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-11 11:29:23 -0300

    feat: tentei realizar a mudança no onClick do button na tela home, porém a rota nao está sendo capitada

---
+++ espectra-front/src/pages/TelaAdicionarPaciente.jsx
@@ -0,0 +1,5 @@
+function TelaAdicionarPaciente() {
+  return <h1>TELA ADICIONAR PACIENTE</h1>;
+}
+
+export default TelaAdicionarPaciente;
--- espectra-front/src/routes/Routes.jsx
+++ espectra-front/src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaInicial from "../pages/TelaInicial";
 import TelaCadastro from "../pages/TelaCadastro";
+import TelaAdicionarPaciente from "../pages/TelaAdicionarPaciente";
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<TelaInicial />} />
       <Route path="/cadastro" element={<TelaCadastro />} />
+      <Route path="/adicionar_paciente" element={<TelaAdicionarPaciente />} />
     </Routes>
   );
 }
