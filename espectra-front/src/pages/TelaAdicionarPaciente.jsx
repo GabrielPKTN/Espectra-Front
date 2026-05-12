7fab48fafdf0756a1fbb55aa15a7767ca65b77c9
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-12 10:14:26 -0300

    responsividade para tablet feita

--- espectra-front/src/pages/TelaAdicionarPaciente.jsx
+++ espectra-front/src/pages/TelaAdicionarPaciente.jsx
@@ -8,7 +8,7 @@ import Button from "../components/Button";
 function TelaAdicionarPaciente() {
   return (
     // div que carrega tudo na tela
-    <div className="m-4 flex flex-col ">
+    <div className="m-4 flex flex-col">
 
       {/*div do header*/}
       <div className="flex justify-between items-center">
@@ -24,18 +24,22 @@ function TelaAdicionarPaciente() {
       </div>
 
       {/*div que carregará o card cinza com informações do paciente. Possivelmente vai ser preciso componentizar.*/}
-      <div className="bg-gray-200 h-auto w-full mt-8 rounded-2xl shadow-lg/20 border border-[#C9C9C9] ">
+      <div className="bg-gray-200 h-auto w-full mt-8 rounded-2xl shadow-lg/20 border border-[#C9C9C9]
+      md:mt-12 md:h-125">
         <div className="flex flex-col justify-center items-center gap-4 mb-4">
 
         <img src={default_photo}
         alt="foto usuario"
-        className="w-20 h-20 mt-5"/>
+        className="w-20 h-20 mt-5
+          md:w-28 md:h-28 md:mt-18"/>
 
-        <h1 className="primary-color font-bold text-2xl instrument-sans">
+        <h1 className="primary-color font-bold text-2xl instrument-sans
+        md:text-3xl md:mt-4">
           NOME USUÁRIO
         </h1>
 
-        <p className="primary-color font-inclusive-sans text-center text-lg p-2 font-medium italic">
+        <p className="primary-color font-inclusive-sans text-center text-lg p-2 font-medium italic
+        md:text-2xl">
         NOME USUÁRIO nasceu em DATA, tem IDADE,
         está na SERIE-ESCOLA e possui diagnóstico de DIAGNOSTICO com grau de suporte X.
         </p>
@@ -57,8 +61,6 @@ function TelaAdicionarPaciente() {
             Adicionar paciente
           </Button>
         </div>
-
-
     </div>
       
   )
