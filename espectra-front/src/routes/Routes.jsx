7558e48c392f61e6651f87be9b960e067d207ec0
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-05-12 17:40:29 -0300

    chore: Ajusta nome dos arquivos referente as telas de atividade

--- espectra-front/src/routes/Routes.jsx
+++ espectra-front/src/routes/Routes.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaInicial from "../pages/TelaInicial";
 import TelaCadastro from "../pages/TelaCadastro";
+import TelaEditarAtividade from "../pages/TelaEditarAtividade";
 import TelaCadastroAtividade from "../pages/TelaCadastroAtividade";
-import TelaCadastroAtividadePersonalizada from "../pages/TelaCadastroAtividadePersonalizada";
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<TelaInicial />} />
       <Route path="/cadastro" element={<TelaCadastro />} />
-      <Route path="/cadastro-atividade" element={<TelaCadastroAtividade />} />
-      <Route path="/cadastro-atividade-personalizada" element={<TelaCadastroAtividadePersonalizada />} />
+      <Route path="/atividade/editar" element={<TelaEditarAtividade />} />
+      <Route path="/atividade/cadastrar" element={<TelaCadastroAtividade />} />
     </Routes>
   );
 }
