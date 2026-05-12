3cc58b4abbdaf8c5aa119b4c36a6563d1e9ac8c3
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-12 10:39:25 -0300

    feat: responsividade finalizada e rota de button de adicionar o paciente já direciona novamente para a tela home. Falta integrar com API agora

--- espectra-front/src/pages/TelaAdicionarPaciente.jsx
+++ espectra-front/src/pages/TelaAdicionarPaciente.jsx
@@ -3,35 +3,49 @@ import logotipo from "../assets/logotipos/logo.png";
 import default_photo from "../assets/general_photos/default-photo.png";
 import InputHome from "../components/input/InputHome";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 
 function TelaAdicionarPaciente() {
+
+  const navigate = useNavigate()
+
   return (
     // div que carrega tudo na tela
     <div className="m-4 flex flex-col">
 
       {/*div do header*/}
-      <div className="flex justify-between items-center">
-      <ChevronLeft className="primary-color size-10"/>
-      <img src={logotipo} alt="logotipo do app" className="size-10 w-auto"/>
-      <img src={default_photo} alt="foto usuario" className="size-12" />
+      <div className="flex justify-between items-center lg:mt-2">
+      <ChevronLeft 
+        className="primary-color size-10
+        lg:size-14"/>
+      <img src={logotipo} alt="logotipo do app" 
+      className="size-10 w-auto
+      lg:size-14 lg:w-auto"/>
+      <img src={default_photo} alt="foto usuario" className="size-12 lg:size-16" />
       </div>
 
       {/*div do input*/}
-      <div className=" flex flex-col mt-12 items-center justify-center gap-12">
-        <p className="text-center text-2xl inclusive-sans font-medium">Digite o CPF do paciente</p>
+      <div className=" flex flex-col mt-12 items-center justify-center gap-12
+      lg:w-162.5 lg:mx-auto lg:gap-8 lg:mt-8">
+        <p className="text-center text-2xl inclusive-sans font-medium
+        lg:text-3xl">
+          Digite o CPF do paciente
+        </p>
         <InputHome></InputHome>
       </div>
 
-      {/*div que carregará o card cinza com informações do paciente. Possivelmente vai ser preciso componentizar.*/}
+      {/*div que carregará o card cinza com informações do paciente.*/}
       <div className="bg-gray-200 h-auto w-full mt-8 rounded-2xl shadow-lg/20 border border-[#C9C9C9]
-      md:mt-12 md:h-125">
+      md:mt-12 md:h-125
+      lg:w-175 lg:mx-auto lg:h-90 lg:mt-10">
         <div className="flex flex-col justify-center items-center gap-4 mb-4">
 
         <img src={default_photo}
         alt="foto usuario"
         className="w-20 h-20 mt-5
-          md:w-28 md:h-28 md:mt-18"/>
+          md:w-28 md:h-28 md:mt-18
+          lg:mt-8 lg:w-25 lg:h-25"/>
 
         <h1 className="primary-color font-bold text-2xl instrument-sans
         md:text-3xl md:mt-4">
@@ -46,7 +60,11 @@ function TelaAdicionarPaciente() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-4 mt-5">        
+      <div className="flex flex-col justify-center items-center gap-4 mt-5
+      lg:flex lg:flex-row">      
+
+      {/*Nestes buttons vai ser necessário adicionar a rota da tela do formulário ou retorná-lo para a tela home,
+        já com o novo paciente adicionado.*/}  
           <Button
             variantClick="basicClick"
             // *onClick={}*/ 
@@ -56,7 +74,7 @@ function TelaAdicionarPaciente() {
 
           <Button
             variantClick="basicClick"
-            // *onClick={}*/ 
+            onClick={()=> navigate("/home")}
           >
             Adicionar paciente
           </Button>
--- espectra-front/src/routes/Routes.jsx
+++ espectra-front/src/routes/Routes.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaInicial from "../pages/TelaInicial";
 import TelaCadastro from "../pages/TelaCadastro";
 import TelaAdicionarPaciente from "../pages/TelaAdicionarPaciente";
+import TelaHome from "../pages/TelaHome";
 
 function Rotas() {
   return (
     <Routes>
       {/*<Route path="/" element={<TelaInicial />} />*/}
      {/*<Route path="/cadastro" element={<TelaCadastro />} />*/}
+     <Route path="/home" element={<TelaHome />} />
       <Route path="/adicionar_paciente" element={<TelaAdicionarPaciente />} />
     </Routes>
   );
