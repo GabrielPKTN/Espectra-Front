8f2a671ba04aa5ad2ec5b858304120da182f5cfd
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-05-13 19:10:49 -0300

    feat: Adiciona modelo de celular completo

---
+++ espectra-front/src/components/CardAtividadeAndamento.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+import setaBaixo from "../assets/general_photos/setaBaixo.svg";
+import Button from "./Button";
+import trash from "../assets/general_photos/trash.svg";
+import pen from "../assets/general_photos/pen.svg";
+
+
+
+export default function CardAtividade() {
+
+    const [expandido, setExpandido] = useState(false);
+
+    return (
+        <div
+            className="
+                flex flex-col
+                bg-[#F9F9F9]
+                px-5 py-3
+                rounded-xl
+                border
+                border-[#C6C6C6]
+                shadow-[0_4px_20px_rgba(0,0,0,0.08)]
+                w-full
+            "
+        >
+
+            <div
+                onClick={() => setExpandido(!expandido)}
+                className="
+                    flex
+                    items-center
+                    justify-between
+                    cursor-pointer
+                "
+            >
+
+                <p className="instrument-sans font-semibold">
+                    Descrição da atividade
+                </p>
+
+                <img
+                    src={setaBaixo}
+                    alt="Expandir card"
+                    className={`
+                        transition-transform duration-300
+                        ${expandido ? "rotate-180" : ""}
+                    `}
+                />
+
+            </div>
+
+            {expandido && (
+                <div className="mt-5 flex flex-col gap-5">
+                    <button className="
+                        instrument-sans font-bold bg-[var(--bg-secondary-color)] p-2 rounded-full text-white"
+
+                        >
+                            Realizar Tentativa
+                    </button>
+
+                    <button className="
+                        instrument-sans font-bold bg-[var(--bg-secondary-color)] p-2 rounded-full text-white"
+
+                        >
+                            Histórico de tentativas
+                    </button>
+
+                    <button className="
+                        instrument-sans font-bold bg-[var(--bg-secondary-color)] p-2 rounded-full text-white"
+
+                        >
+                            Declarar Habilidade
+                    </button>
+
+
+                    <div className="flex gap-8 mt-4 justify-center">
+
+                        <div className="flex items-center justify-center gap-1">
+                            <img src={trash} alt="Excluir" className="w-7"/>
+                            <span className="instrument-sans text-sm text-[#F94C4C] text-xs">
+                                Excluir atividade
+                            </span>
+                        </div>
+
+                        <div className="flex items-center justify-center gap-1">
+                            <img src={pen} alt="Editar" className="w-7"/>
+                            <span className="instrument-sans text-sm text-[var(--bg-secondary-color)] text-xs">
+                                Editar atividade
+                            </span>
+                        </div>
+
+                    </div>
+
+
+                </div>
+            )}
+
+        </div>
+    )
+}
--- espectra-front/src/pages/TelaAtividades.jsx
+++ espectra-front/src/pages/TelaAtividades.jsx
@@ -1,10 +1,11 @@
-import CardAtividade from "../components/CardAtividade"
+import CardAtividade from "../components/CardAtividadeAndamento"
+import CardAtividadeAdquirida from "../components/CardAtividadeAdquirida"
 import HeaderUsuario from "../components/HeaderUsuario"
 import OutlinedTitle from "../components/OutlinedTitle"
 
 function TelaAtividades(){
     return(
-        <div className="h-screen w-full">
+        <div className="h-screen w-full flex flex-col px-10 gap-6">
 
             <div>
                 <HeaderUsuario/>
@@ -16,7 +17,7 @@ function TelaAtividades(){
             </div>
            
 
-            <div className="justify-items-center">
+            <div className="flex flex-col justify-items-center items-center gap-5">
                 <h2 className="
                     instrument-sans font-bold text-lg"
                     >
@@ -24,15 +25,23 @@ function TelaAtividades(){
                 </h2>
 
                 <CardAtividade/>
+
+                <CardAtividade/>
             </div>
 
-            <div className="justify-items-center">
+            <div className="flex flex-col justify-items-center items-center gap-5">
                 <h2 className="
                     instrument-sans font-bold text-lg"
                     >
                         Habilidades adquiridas
                 </h2>
+
+                <CardAtividadeAdquirida/>
+
+                <CardAtividadeAdquirida/>
             </div>
+
+            <button className="bg-[var(--bg-secondary-color)] text-white p-2 rounded-full instrument-sans font-bold ">Adicionar atividade</button>
 
             
         </div>
