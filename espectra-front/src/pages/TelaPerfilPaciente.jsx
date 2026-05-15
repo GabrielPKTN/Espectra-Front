486710bcf78d0574b16624c9f3c8e6ac658f429a
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-15 09:46:36 -0300

    feat: corrigi layout que estava quebrado para centralizar. Funcionando totalmente agora, falta apenas integrar com API.

--- espectra-front/src/pages/TelaPerfilPaciente.jsx
+++ espectra-front/src/pages/TelaPerfilPaciente.jsx
@@ -69,164 +69,171 @@ function TelaPerfilPaciente() {
   const isDesktop = barraGraficoDesktop();
 
   return (
-    <div
-      className="flex flex-col w-screen h-auto p-2 
-        lg:bg-[#dfedff]"
-    >
+    <div className="lg:bg-[#dfedff] flex flex-col justify-between gap-2 lg:h-screen lg:overflow-hidden">
       {/*HEADER*/}
-      <div className="flex flex-row justify-between p-2">
+      <div className="flex flex-row justify-between p-2 m-2">
         <ChevronLeft className="primary-color size-8 lg:size-12" />
         <CircleUser className="primary-color size-10 lg:size-14"></CircleUser>
       </div>
 
-      {/*MAIN*/}
       <div
-        className="flex flex-col justify-center items-center
-        lg:bg-white lg:p-4 lg:m-6 lg:rounded-4xl"
+        className="flex flex-col w-screen h-fit p-2 
+        lg:bg-[#dfedff] lg:h-screen lg:justify-start lg:items-center lg:overflow-hidden
+        xl:bg-[#dfedff] xl:h-screen"
       >
-        {/*Nome e foto do paciente.*/}
-        <div className="flex flex-col items-center gap-3 lg:mt-4">
-          <CircleUser className="primary-color size-24 lg:size-28"></CircleUser>
+        {/*MAIN*/}
+        <div
+          className="flex flex-col justify-center items-center
+        lg:bg-white lg:p-4 lg:m-6 lg:rounded-4xl lg:max-w-max"
+        >
+          {/*Nome e foto do paciente.*/}
+          <div className="flex flex-col items-center gap-3 lg:mt-4">
+            <CircleUser className="primary-color size-24 lg:size-28"></CircleUser>
 
-          <h1
-            className="primary-color font-bold instrument-sans uppercase text-xl
+            <h1
+              className="primary-color font-bold instrument-sans uppercase text-xl
           md:text-2xl
           lg:text-3xl lg:mt-4"
-          >
-            Nome do paciente
-          </h1>
-        </div>
+            >
+              Nome do paciente
+            </h1>
+          </div>
 
-        <p
-          className="text-center italic primary-color inclusive-san font-lights
+          <p
+            className="text-center italic primary-color inclusive-san font-lights
               md:text-lg md:px-10
               lg:mt-6 lg:text-[22px] lg:px-30"
-        >
-          Nome do paciente nasceu em Data, tem Idade, está na 5° Série e possui
-          diagnóstico de Diagnostico com grau de suporte X.
-        </p>
+          >
+            Nome do paciente nasceu em Data, tem Idade, está na 5° Série e
+            possui diagnóstico de Diagnostico com grau de suporte X.
+          </p>
 
-        <div
-          className="flex flex-row gap-1 font-semibold instrument-sans text-base p-3 justify-center
+          <div
+            className="flex flex-row gap-1 font-semibold instrument-sans text-base p-3 justify-center
               md:text-lg
               lg:text-[22px] lg:p-4"
-        >
-          <p className="primary-color">Telefone do responsável:</p>
-          <p className="text-gray-700">+55 11 91234-5678</p>
-        </div>
-
-        <div className="flex flex-col w-screen lg:grid lg:grid-cols-2 lg:gap-x-10 lg:w-full">
-          <div
-            className="mt-5 flex flex-col 
-            lg:order-2"
           >
-            <h2 className="secondary-color font-semibold text-2xl instrument-sans text-center">
-              Gráfico de desempenho
-            </h2>
+            <p className="primary-color">Telefone do responsável:</p>
+            <p className="text-gray-700">+55 11 91234-5678</p>
+          </div>
 
-            {/*GRAFICO*/}
-            <div className="h-64 w-full mt-4">
-              <ResponsiveContainer width="100%" height="100%">
-                <BarChart
-                  data={data}
-                  margin={{
-                    top: 35,
-                    right: 30,
-                    left: 20,
-                    bottom: 0,
-                  }}
-                >
-                  <XAxis dataKey="nome" tick={false} />
-                  <YAxis hide domain={[0, 6]} allowDataOverflow={false} />
-                  <Tooltip cursor={{ fill: "transparent" }} />
+          <div className="flex flex-col w-screen lg:grid lg:grid-cols-2 lg:gap-x-10 lg:w-full">
+            <div
+              className="mt-5 flex flex-col 
+            lg:order-2"
+            >
+              <h2 className="secondary-color font-semibold text-2xl instrument-sans text-center">
+                Gráfico de desempenho
+              </h2>
 
-                  <Bar dataKey="idade" barSize={isDesktop ? 65 : 44}>
-                    {data.map((entry, index) => (
-                      <Cell key={`cell-${index}`} fill={entry.cor} />
-                    ))}
+              {/*GRAFICO*/}
+              <div className="h-64 w-full mt-4">
+                <ResponsiveContainer width="100%" height="100%">
+                  <BarChart
+                    data={data}
+                    margin={{
+                      top: 35,
+                      right: 30,
+                      left: 20,
+                      bottom: 0,
+                    }}
+                  >
+                    <XAxis dataKey="nome" tick={false} />
+                    <YAxis hide domain={[0, 6]} allowDataOverflow={false} />
+                    <Tooltip cursor={{ fill: "transparent" }} />
 
-                    <LabelList
-                      dataKey="idade"
-                      position="top"
-                      offset={10}
-                      className="instrument-sans"
-                      formatter={(value) =>
-                        `${value} ${value === 1 ? "ano" : "anos"}`
-                      }
-                      style={{
-                        fill: "#000000",
-                        fontSize: "12px",
-                        fontWeight: "bold",
-                      }}
-                    />
-                  </Bar>
-                </BarChart>
-              </ResponsiveContainer>
+                    <Bar dataKey="idade" barSize={isDesktop ? 65 : 44}>
+                      {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={entry.cor} />
+                      ))}
+
+                      <LabelList
+                        dataKey="idade"
+                        position="top"
+                        offset={10}
+                        className="instrument-sans"
+                        formatter={(value) =>
+                          `${value} ${value === 1 ? "ano" : "anos"}`
+                        }
+                        style={{
+                          fill: "#000000",
+                          fontSize: "12px",
+                          fontWeight: "bold",
+                        }}
+                      />
+                    </Bar>
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+
+            {/*Lista de habilidades. NECESSÁRIO ADICIONAR OS ONCLICK QUE DIRECIONAM PARA A TELA DAS RESPECTIVAS HABILIDADES.*/}
+            <div className="px-8 mt-6 w-full flex flex-col justify-center gap-4">
+              <h2 className="text-center font-semibold instrument-sans secondary-color text-2xl">
+                Habilidades
+              </h2>
+              <ButtonHabilidade
+                color="cor-socializacao"
+                colorHover="cor-socializacao-hover"
+                type="button"
+              >
+                Socialização
+              </ButtonHabilidade>
+
+              <ButtonHabilidade
+                color="cor-cognicao"
+                colorHover="cor-cognicao-hover"
+                type="button"
+              >
+                Cognição
+              </ButtonHabilidade>
+
+              <ButtonHabilidade
+                color="cor-autocuidado"
+                colorHover="cor-autocuidado-hover"
+                type="button"
+              >
+                Auto-cuidado
+              </ButtonHabilidade>
+
+              <ButtonHabilidade
+                color="cor-linguagem"
+                colorHover="cor-linguagem-hover"
+                type="button"
+              >
+                Linguagem
+              </ButtonHabilidade>
+
+              <ButtonHabilidade
+                color="cor-devmotor"
+                colorHover="cor-devmotor-hover"
+                type="button"
+              >
+                Desenvolvimento-motor
+              </ButtonHabilidade>
             </div>
           </div>
 
-          {/*Lista de habilidades. NECESSÁRIO ADICIONAR OS ONCLICK QUE DIRECIONAM PARA A TELA DAS RESPECTIVAS HABILIDADES.*/}
-          <div className="px-8 mt-6 w-full flex flex-col justify-center gap-4">
-            <h2 className="text-center font-semibold instrument-sans secondary-color text-2xl">
-              Habilidades
-            </h2>
-            <ButtonHabilidade
-              color="cor-socializacao"
-              colorHover="cor-socializacao-hover"
-              type="button"
-            >
-              Socialização
-            </ButtonHabilidade>
-
-            <ButtonHabilidade
-              color="cor-cognicao"
-              colorHover="cor-cognicao-hover"
-              type="button"
-            >
-              Cognição
-            </ButtonHabilidade>
-
-            <ButtonHabilidade
-              color="cor-autocuidado"
-              colorHover="cor-autocuidado-hover"
-              type="button"
-            >
-              Auto-cuidado
-            </ButtonHabilidade>
-
-            <ButtonHabilidade
-              color="cor-linguagem"
-              colorHover="cor-linguagem-hover"
-              type="button"
-            >
-              Linguagem
-            </ButtonHabilidade>
-
-            <ButtonHabilidade
-              color="cor-devmotor"
-              colorHover="cor-devmotor-hover"
-              type="button"
-            >
-              Desenvolvimento-motor
-            </ButtonHabilidade>
-          </div>
-        </div>
-
-        <div
-          className="flex flex-col gap-4 mt-16 items-center lg:flex-row
+          <div
+            className="flex flex-col gap-4 mt-16 items-center lg:flex-row
         lg:w-175 lg:m-8 lg:mt-12"
-        >
-          <Button
-            variantClick="basicClick"
-            type="button"
-            className="w-full p-3"
           >
-            Editar avaliação de desempenho
-          </Button>
+            <Button
+              variantClick="basicClick"
+              type="button"
+              className="w-full p-3"
+            >
+              Editar avaliação de desempenho
+            </Button>
 
-          <Button variantClick="deleteButton" type="button" className="w-full">
-            Remover paciente
-          </Button>
+            <Button
+              variantClick="deleteButton"
+              type="button"
+              className="w-full"
+            >
+              Remover paciente
+            </Button>
+          </div>
         </div>
       </div>
     </div>
