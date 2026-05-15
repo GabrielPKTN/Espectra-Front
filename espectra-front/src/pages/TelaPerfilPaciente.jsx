09a0394bb649ecf386b09c8e4c083449e0f06b05
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-05-15 08:54:43 -0300

    feat: tela de perfil de usuário finalizado com responsividade.

--- espectra-front/src/pages/TelaPerfilPaciente.jsx
+++ espectra-front/src/pages/TelaPerfilPaciente.jsx
@@ -15,6 +15,7 @@ import {
 } from "recharts";
 import ButtonHabilidade from "../components/ButtonHabilidade";
 import Button from "../components/Button";
+import { useEffect, useState } from "react";
 
 function TelaPerfilPaciente() {
   const data = [
@@ -50,8 +51,28 @@ function TelaPerfilPaciente() {
     },
   ];
 
+  // função que faz o gráfico ficar maior na tela de computador, considerando o tamanho a partir de 768px.
+  const barraGraficoDesktop = () => {
+    const [isDesktop, setIsDesktop] = useState(false);
+
+    useEffect(() => {
+      const media = window.matchMedia("(min-width: 768px)");
+      setIsDesktop(media.matches);
+
+      const listener = (e) => setIsDesktop(e.matches);
+      media.addEventListener("change", listener);
+    }, []);
+
+    return isDesktop;
+  };
+
+  const isDesktop = barraGraficoDesktop();
+
   return (
-    <div className=" flex flex-col w-screen h-auto p-2 ">
+    <div
+      className="flex flex-col w-screen h-auto p-2 
+        lg:bg-[#dfedff]"
+    >
       {/*HEADER*/}
       <div className="flex flex-row justify-between p-2">
         <ChevronLeft className="primary-color size-8 lg:size-12" />
@@ -59,39 +80,46 @@ function TelaPerfilPaciente() {
       </div>
 
       {/*MAIN*/}
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center
+        lg:bg-white lg:p-4 lg:m-6 lg:rounded-4xl"
+      >
         {/*Nome e foto do paciente.*/}
-        <div className="flex flex-col items-center gap-3">
+        <div className="flex flex-col items-center gap-3 lg:mt-4">
           <CircleUser className="primary-color size-24 lg:size-28"></CircleUser>
 
           <h1
             className="primary-color font-bold instrument-sans uppercase text-xl
           md:text-2xl
-          lg:text-3xl"
+          lg:text-3xl lg:mt-4"
           >
             Nome do paciente
           </h1>
         </div>
 
-        <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-x-10 ">
-          <div className="mt-5 flex flex-col ">
-            {/*descricao e numero. */}
-            <p
-              className="text-center italic primary-color inclusive-san font-lights
-              md:text-lg md:px-10"
-            >
-              Nome do paciente nasceu em Data, tem Idade, está na 5° Série e
-              possui diagnóstico de Diagnostico com grau de suporte X.
-            </p>
+        <p
+          className="text-center italic primary-color inclusive-san font-lights
+              md:text-lg md:px-10
+              lg:mt-6 lg:text-[22px] lg:px-30"
+        >
+          Nome do paciente nasceu em Data, tem Idade, está na 5° Série e possui
+          diagnóstico de Diagnostico com grau de suporte X.
+        </p>
 
-            <div
-              className="flex flex-row gap-1 font-semibold instrument-sans text-base p-3 justify-center
-              md:text-lg"
-            >
-              <p className="primary-color">Telefone do responsável:</p>
-              <p>+55 11 91234-5678</p>
-            </div>
+        <div
+          className="flex flex-row gap-1 font-semibold instrument-sans text-base p-3 justify-center
+              md:text-lg
+              lg:text-[22px] lg:p-4"
+        >
+          <p className="primary-color">Telefone do responsável:</p>
+          <p className="text-gray-700">+55 11 91234-5678</p>
+        </div>
 
+        <div className="flex flex-col w-screen lg:grid lg:grid-cols-2 lg:gap-x-10 lg:w-full">
+          <div
+            className="mt-5 flex flex-col 
+            lg:order-2"
+          >
             <h2 className="secondary-color font-semibold text-2xl instrument-sans text-center">
               Gráfico de desempenho
             </h2>
@@ -112,7 +140,7 @@ function TelaPerfilPaciente() {
                   <YAxis hide domain={[0, 6]} allowDataOverflow={false} />
                   <Tooltip cursor={{ fill: "transparent" }} />
 
-                  <Bar dataKey="idade" barSize={44}>
+                  <Bar dataKey="idade" barSize={isDesktop ? 65 : 44}>
                     {data.map((entry, index) => (
                       <Cell key={`cell-${index}`} fill={entry.cor} />
                     ))}
@@ -184,7 +212,10 @@ function TelaPerfilPaciente() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-4 mt-16 items-center lg:flex-row">
+        <div
+          className="flex flex-col gap-4 mt-16 items-center lg:flex-row
+        lg:w-175 lg:m-8 lg:mt-12"
+        >
           <Button
             variantClick="basicClick"
             type="button"
