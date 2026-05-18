0eba054bea117af6fca70ff7e2a9abfa6fbdee5e
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1241955.cfp127aluno.local>
2026-05-18 09:42:25 -0300

    feat: Conclui tela de atualizar perfil de psicopedagogo com responsividade

--- espectra-front/src/pages/TelaAtualizarPerfilPsicopedagogo.jsx
+++ espectra-front/src/pages/TelaAtualizarPerfilPsicopedagogo.jsx
@@ -5,7 +5,7 @@ import Button from "../components/Button";
 import { ChevronLeft } from "lucide-react";
 import { BadgePlus } from "lucide-react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { data, useNavigate } from "react-router-dom";
 
 function AtualizarPerfilPsicopedagogo() {
 
@@ -27,6 +27,152 @@ function AtualizarPerfilPsicopedagogo() {
         alert("Suas alterações foram salvas com sucesso!")
 
         navigate("/")
+    }
+
+    function validarNome(e) {
+        const valorInserido = e.target.value
+
+        setNome(valorInserido)
+
+        const nomeLimpo = valorInserido.trim()
+
+        if (!nomeLimpo) {
+            setErroNome("Seu nome não pode estar vazio!")
+            return
+        }
+
+        if (/\d/.test(nomeLimpo)) {
+            setErroNome("O nome não pode conter números!")
+            return
+        }
+
+        if (/[^a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]/.test(nomeLimpo)) {
+            setErroNome("O nome não pode conter caracteres especiais!")
+            return
+        }
+
+        setErroNome("")
+    }
+
+    function validarEmail(e) {
+        const valorInserido = e.target.value
+
+        setEmail(valorInserido)
+
+        const emailLimpo = valorInserido.trim()
+
+        if (!emailLimpo) {
+            setErroEmail("Seu email não pode estar vazio")
+        }
+
+        const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        if (!emailValido.test(emailLimpo)) {
+            setErroEmail("Digite um email válido!")
+            return
+        }
+    }
+
+    function mascaraTelefone(e) {
+        const valorInserido = e.target.value
+
+        let telefoneLimpo = valorInserido.replace(/\D/g, "")
+
+        if (telefoneLimpo.length <= 10) {
+            telefoneLimpo = telefoneLimpo.replace(/ˆ(\d{2})(\d)/g, "($1) $2")
+            telefoneLimpo = telefoneLimpo.replace(/(\d{2})(\d)/g, "($1) $2")
+        } else {
+            telefoneLimpo = telefoneLimpo.replace(/^(\d{2})(\d)/g, "($1) $2");
+            telefoneLimpo = telefoneLimpo.replace(/(\d{5})(\d)/, "$1-$2")
+        }
+
+        return telefoneLimpo
+    }
+
+    function validarNumeroTelefone(e) {
+        const telefoneComMascara = mascaraTelefone(e)
+
+        setTelefone(telefoneComMascara)
+
+        const telefoneLimpo = telefoneComMascara.replace(/\D/g, "")
+
+        if (!telefoneLimpo) {
+            setErroTelefone("Informe o seu número de telefone!")
+            return
+        }
+
+        const formatoTelefone = /^[1-9]{2}(?:[1-8]|9)[0-9]{7,8}$/
+
+        if (!formatoTelefone.test(telefoneLimpo)) {
+            setErroTelefone("Digite um número válido!")
+            return
+        }
+
+        setErroTelefone("")
+    }
+
+    function mascaraDataNascimento(e){
+        const valorInserido = e.target.value
+
+        let dataLimpa = valorInserido.replace(/\D/g, "")
+
+        dataLimpa = dataLimpa.replace(/^(\d{2})(\d)/, "$1/$2")
+        dataLimpa = dataLimpa.replace(/^(\d{2})\/(\d{2})(\d)/, "$1/$2/$3")
+
+        return dataLimpa
+    }
+
+    function validarDataNascimento(e){
+        const dataComMascara = mascaraDataNascimento(e)
+
+        setNascimento(dataComMascara)
+
+        const dataLimpa = dataComMascara.replace(/\D/g, "")
+
+        if(!dataLimpa){
+            setErroNascimento("Informe sua data de nascimento!")
+            return
+        }
+
+        if(dataLimpa.length < 8){
+            setErroNascimento("Digite uma data válida!")
+            return
+        }
+
+        const dia = parseInt(dataLimpa.substring(0,2))
+        const mes = parseInt(dataLimpa.substring(2,4))
+        const ano = parseInt(dataLimpa.substring(4,8))
+
+        const dataAtual = new Date()
+        const anoAtual = dataAtual.getFullYear()
+
+        if(dia < 1 || dia > 31){
+            setErroNascimento("Dia Inválido")
+            return
+        }
+
+        if(mes < 1 || mes > 12){
+            setErroNascimento("Mês inválido!")
+            return
+        }
+
+        if(ano < 1900 || ano > anoAtual){
+            setErroNascimento("Ano inválido!")
+            return
+        }
+
+        const dataValida = new Date(ano, mes - 1, dia)
+
+        if(
+            dataValida.getDate() !== dia ||
+            dataValida.getMonth() !== mes - 1 ||
+            dataValida.getFullYear() !== ano
+        ){
+            setErroNascimento("Data inválida!")
+            return
+        }
+
+        setErroNascimento("")
     }
 
     return (
@@ -67,7 +213,7 @@ function AtualizarPerfilPsicopedagogo() {
             </div>
 
             {/* Div que guarda os inputs do Psicopedagogo */}
-            <div className="flex flex-col justify-center items-center gap-4 mt-4 w-full lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-6">
+            <div className="flex flex-col justify-center items-center gap-4 mt-4 w-full md:mt-20">
                 <div className="flex flex-col gap-2">
                     <label className="text-left text-[#3277CF] text-[18px] font-semibold">
                         Nome
@@ -75,10 +221,18 @@ function AtualizarPerfilPsicopedagogo() {
 
                     <InputDefault
                         type="text"
-                        variantInput="basicInput"
+                        variantInput={erroNome ? "errorInput" : "basicInput"}
                         value={nome}
-                        className="!w-[300px]"
+                        onChange={validarNome}
+                        className="!w-[300px] md:!w-[500px] lg:!w-[550px]"
                     />
+                    {
+                        erroNome && (
+                            <span className="text-red-500 text-sm">
+                                {erroNome}
+                            </span>
+                        )
+                    }
 
                     <label className="text-left text-[#3277CF] text-[18px] font-semibold">
                         Email
@@ -86,10 +240,18 @@ function AtualizarPerfilPsicopedagogo() {
 
                     <InputDefault
                         type="text"
-                        variantInput="basicInput"
+                        variantInput={erroEmail ? "errorInput" : "basicInput"}
                         value={email}
-                        className="!w-[300px]"
+                        onChange={validarEmail}
+                        className="!w-[300px] md:!w-[500px] lg:!w-[550px]"
                     />
+                    {
+                        erroEmail && (
+                            <span className="text-red-500 text-sm">
+                                {erroEmail}
+                            </span>
+                        )
+                    }
                 </div>
 
                 <div className="flex flex-row gap-6">
@@ -100,10 +262,18 @@ function AtualizarPerfilPsicopedagogo() {
 
                         <InputDefault
                             type="text"
-                            variantInput="basicInput"
+                            variantInput={erroTelefone ? "errorInput" : "basicInput"}
                             value={telefone}
-                            className="!w-[140px]"
+                            onChange={validarNumeroTelefone}
+                            className="!w-[140px] md:!w-[240px] lg:!w-[265px]"
                         />
+                        {
+                            erroTelefone && (
+                                <span className="text-red-500 text-sm">
+                                    {erroTelefone}
+                                </span>
+                            )
+                        }
                     </div>
 
                     <div className="flex flex-col gap-2">
@@ -113,26 +283,35 @@ function AtualizarPerfilPsicopedagogo() {
 
                         <InputDefault
                             type="text"
-                            variantInput="basicInput"
+                            variantInput={erroNascimento ? "errorInput" : "basicInput"}
                             placeholder="DD/MM/AAAA"
                             value={nascimento}
-                            className="!w-[140px]"
+                            onChange={validarDataNascimento}
+                            limiteCaracteres={10}
+                            className="!w-[140px] md:!w-[240px] lg:!w-[265px]"
                         />
+                        {
+                            erroNascimento && (
+                                <span className="text-red-500 text-sm">
+                                    {erroNascimento}
+                                </span>
+                            )
+                        }
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center gap-4">
+                <div className="flex flex-col justify-center items-center gap-4 md:mt-15 lg:flex-row lg:gap-6">
                     <Button
                         variantClick="editButton"
-                        className="text-[#3277CF] cursor-pointer transform-gpu transform-all duration-300 ease-in-out hover:scale-110"
+                        className="text-[#3277CF] cursor-pointer transform-gpu transform-all duration-300 ease-in-out hover:scale-110 md:w-[300px]"
                         onClick=""// Navigate para a tela de redefinir senha 
                     >
                         Editar senha
                     </Button>
 
                     <Button
-                    variantClick="editButton"
-                        className="text-[#3277CF] cursor-pointer transform-gpu transform-all duration-300 ease-in-out hover:scale-110"
+                        variantClick="editButton"
+                        className="text-[#3277CF] cursor-pointer transform-gpu transform-all duration-300 ease-in-out hover:scale-110 md:w-[300px]"
                         onClick={alteracoesSalvas}
                     >
                         Salvar alterações
