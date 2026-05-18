ee147379d862f9b7e0dbf4879f20eca5d3da0f96
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-05-18 20:13:08 -0300

    feat: Adicionando integração com a API na tela home

--- espectra-front/src/components/card-paciente/CardPaciente.jsx
+++ espectra-front/src/components/card-paciente/CardPaciente.jsx
@@ -1,21 +1,30 @@
+import { useEffect, useMemo } from "react";
 import ContainerPacientePhoto from "./ContainerPacientePhoto"
 import LabelInfoCard from "./LabelInfoCard"
 
 function CardPaciente(props) {
 
+    const diagnostico = useMemo(() => {
+
+        return props.diagnostico_breve.map(
+            diag => diag.sigla
+        ).join(" ")
+
+    }, [props.diagnostico_breve])
+
     return <div key={props.id} className="
     bg-white shadow-lg w-[90%] h-full max-h-35 justify-between items-center rounded-3xl p-2 flex border-2 border-(--bg-primary-color) md:w-full">
 
-        <ContainerPacientePhoto/>
+        <ContainerPacientePhoto foto={props.foto} />
         <div className="flex flex-col w-full justify-center">
             <span className="
             text-(--bg-primary-color) font-instrument-sans font-bold w-full text-xl p-1
             ">{props.nome}</span>
             <div className="p-1 flex justify-start flex-row h-auto gap-1 flex-wrap">
                 <LabelInfoCard info={`${props.idade} ANOS`} ></LabelInfoCard>
-                <LabelInfoCard info={props.numero_registro} ></LabelInfoCard>
+                <LabelInfoCard info={props.cpf} ></LabelInfoCard>
                 <LabelInfoCard info={props.serie_escolar} ></LabelInfoCard>
-                <LabelInfoCard info={props.diagnostico} ></LabelInfoCard>
+                <LabelInfoCard info={diagnostico} ></LabelInfoCard>
                 <LabelInfoCard info={props.grau_suporte} ></LabelInfoCard>
             </div>
         </div>
--- espectra-front/src/pages/TelaHome.jsx
+++ espectra-front/src/pages/TelaHome.jsx
@@ -2,173 +2,110 @@ import InputHome from "../components/input/InputHome.jsx";
 import ContainerHeader from "../components/ContainerHeader";
 import ContainerPacientes from "../components/ContainerPacientes.jsx";
 import Button from "../components/Button.jsx";
+import app from "../services/api.js"
 import { Filter } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TelaHome() {
-  const navigate = useNavigate();
 
-  const [pacientes, setPacientes] = useState([
-    {
-      id: 2,
-      foto: "http://azure.blob.img/paciente2.jpg",
-      nome: "Ana Beatriz Oliveira",
-      data_nascimento: "2015-05-22",
-      idade: 8,
-      diagnostico: "TEA",
-      serie_escolar: "3º Ano",
-      grau_suporte: "Grau 1",
-      numero_registro: 2026040002,
-    },
-    {
-      id: 3,
-      foto: "http://azure.blob.img/paciente3.jpg",
-      nome: "Lucas Henrique Souza",
-      data_nascimento: "2012-11-30",
-      idade: 11,
-      diagnostico: "TDAH e TOD",
-      serie_escolar: "5º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040003,
-    },
-    {
-      id: 4,
-      foto: "http://azure.blob.img/paciente4.jpg",
-      nome: "Mariana Costa Silva",
-      data_nascimento: "2014-03-15",
-      idade: 12,
-      diagnostico: "Autismo",
-      serie_escolar: "6º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040004,
-    },
-    {
-      id: 5,
-      foto: "http://azure.blob.img/paciente5.jpg",
-      nome: "Pedro Alencar Gomes",
-      data_nascimento: "2017-09-08",
-      idade: 8,
-      diagnostico: "TDAH",
-      serie_escolar: "3º Ano",
-      grau_suporte: "Grau 1",
-      numero_registro: 2026040005,
-    },
-    {
-      id: 6,
-      foto: "http://azure.blob.img/paciente6.jpg",
-      nome: "Sofia Rocha Mendes",
-      data_nascimento: "2011-07-21",
-      idade: 14,
-      diagnostico: "Dislexia",
-      serie_escolar: "8º Ano",
-      grau_suporte: "Grau 1",
-      numero_registro: 2026040006,
-    },
-    {
-      id: 7,
-      foto: "http://azure.blob.img/paciente7.jpg",
-      nome: "Gabriel Neves Cruz",
-      data_nascimento: "2016-01-30",
-      idade: 10,
-      diagnostico: "TEA e TDAH",
-      serie_escolar: "4º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040007,
-    },
-    {
-      id: 8,
-      foto: "http://azure.blob.img/paciente8.jpg",
-      nome: "Beatriz Farias Lima",
-      data_nascimento: "2013-11-12",
-      idade: 12,
-      diagnostico: "TOD",
-      serie_escolar: "7º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040008,
-    },
-    {
-      id: 9,
-      foto: "http://azure.blob.img/paciente9.jpg",
-      nome: "Enzo Gabriel Santos",
-      data_nascimento: "2019-05-04",
-      idade: 7,
-      diagnostico: "Autismo Grau 3",
-      serie_escolar: "1º Ano",
-      grau_suporte: "Grau 3",
-      numero_registro: 2026040009,
-    },
-    {
-      id: 10,
-      foto: "http://azure.blob.img/paciente7.jpg",
-      nome: "Gabriel Neves Cruz",
-      data_nascimento: "2016-01-30",
-      idade: 10,
-      diagnostico: "TEA e TDAH",
-      serie_escolar: "4º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040007,
-    },
-    {
-      id: 11,
-      foto: "http://azure.blob.img/paciente8.jpg",
-      nome: "Beatriz Farias Lima",
-      data_nascimento: "2013-11-12",
-      idade: 12,
-      diagnostico: "TOD",
-      serie_escolar: "7º Ano",
-      grau_suporte: "Grau 2",
-      numero_registro: 2026040008,
-    },
-    {
-      id: 12,
-      foto: "http://azure.blob.img/paciente9.jpg",
-      nome: "Enzo Gabriel Santos",
-      data_nascimento: "2019-05-04",
-      idade: 7,
-      diagnostico: "Autismo Grau 3",
-      serie_escolar: "1º Ano",
-      grau_suporte: "Grau 3",
-      numero_registro: 2026040009,
-    },
-  ]);
+    const navigate = useNavigate();
 
-  const [busca, setBusca] = useState("");
+    const [pacientes, setPacientes] = useState([]);
+    const [busca, setBusca] = useState("");
 
-  const pacientesFiltrados = pacientes.filter((paciente) =>
-    paciente.nome.toLowerCase().includes(busca.toLowerCase()),
-  );
+    const pacientesFiltrados = pacientes.filter((paciente) =>
+        paciente.nome.toLowerCase().includes(busca.toLowerCase()),
+    );
 
-  console.log(busca);
-  console.log(pacientesFiltrados);
+    console.log(pacientesFiltrados)
 
-  return (
-    <div className="py-4 px-5 gap-8 flex flex-col h-screen">
-      <ContainerHeader />
-      <main
-        className="
+    localStorage.setItem(
+
+        "id_usuario", 1
+
+    )
+
+    localStorage.setItem(
+
+        "token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTc3OTEzNjcwNiwiZXhwIjoxMDAwMDE3NzkxMzY3MDZ9.Ao3CYRUsrq-Wn9cHhobuEc3kRsTibpzdXTayJFVc8xE"
+
+    )
+
+    const requestData = async () => {
+
+        try {
+
+            const result = await app.get(`/v1/espectra/usuario/home/${localStorage.getItem('id_usuario')}`,
+                {
+                    headers: {
+                        'x-access-token': localStorage.getItem('token')
+                    }
+                }
+            )
+
+            let rawData = result.data
+
+            const jsonHome = JSON.stringify(rawData)
+            localStorage.setItem("home", jsonHome)
+
+            if (rawData.items.tipo_usuario === 'Psicopedagogo') {
+                setPacientes(rawData.items.pacientes)
+            } else {
+                setPacientes(rawData.items.familiares)
+            }
+
+
+
+        } catch (error) {
+            return false
+        }
+
+    }
+
+    const returnFoto = () => {
+
+        const json = JSON.parse(localStorage.getItem('home'))
+
+        if (json.items.foto) {
+            return json.items.foto
+        } else {
+            return null
+        }
+
+    }
+
+    useEffect(() => {
+        requestData()
+    }, [])
+
+    return (
+        <div className="py-4 px-5 gap-8 flex flex-col h-screen">
+            <ContainerHeader foto={returnFoto()} />
+            <main
+                className="
         gap-2 flex flex-col justify-center items-center grow md:gap-8
         "
-      >
-        <div className="w-full flex gap-2 items-center justify-center">
-          <InputHome busca={setBusca} />
-          <Filter className="text-(--bg-primary-color) size-8" />
-        </div>
-        <ContainerPacientes
-          pacientes={
-            pacientesFiltrados.length > 0 ? pacientesFiltrados : pacientes
-          }
-        />
+            >
+                <div className="w-full flex gap-2 items-center justify-center">
+                    <InputHome busca={setBusca} />
+                    <Filter className="text-(--bg-primary-color) size-8" />
+                </div>
+                <ContainerPacientes
+                    pacientes={
+                        pacientesFiltrados.length > 0 ? pacientesFiltrados : pacientes
+                    }
+                />
 
-        <Button
-          variantClick="basicClick"
-          onClick={() => navigate("/adicionar_paciente")}
-        >
-          Adicionar paciente
-        </Button>
-      </main>
-    </div>
-  );
+                <Button
+                    variantClick="basicClick"
+                    onClick={() => navigate("/adicionar_paciente")}
+                >
+                    Adicionar paciente
+                </Button>
+            </main>
+        </div>
+    );
 }
 
 export default TelaHome;
