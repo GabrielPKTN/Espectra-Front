ccfc7d36a8ce284337fe8579ce54d2ced569a691
EnzoCarrilho <enzocarrilho25@gmail.com>
2026-05-22 10:11:04 -0300

    feat: Adiciona validações e mensagens de sucesso/erro

---
+++ espectra-front/src/pages/ModalValorMeses.jsx
@@ -0,0 +1,36 @@
+import Card from "../components/Card";
+import Button from "../components/Button";
+import fechar from "../assets/general_photos/icons8-close-48.png";
+import light from "../assets/general_photos/Light.svg"
+
+function ModalMeses({ onConfirm, onCancel }) {
+
+    return (
+        <div className="fixed inset-0 w-screen h-screen backdrop-blur-sm bg-black/30 flex justify-center items-center z-50">
+
+            <div className="relative bg-white rounded-xl p-10 w-[80%] md:w-[60%] h-[40%] md:h-auto">
+
+                <img
+                    src={fechar}
+                    alt="Fechar"
+                    onClick={onCancel}
+                    className="w-10 h-10 cursor-pointer absolute top-2 right-2 transition hover:scale-125 lg:size-12"
+                />
+
+                <div className="p-6 flex flex-col items-center gap-5">
+
+                    <img src={light} alt="" className="flex self-center lg:w-14"/>
+                    
+                    <p className="text-md md:text-xl lg:text-3xl text-center font-inclusive-sans">
+                    Esse será a quantidade em meses que será adicionado a idade do paciente assim que essa atividade ser concluída.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ModalMeses;
--- espectra-front/src/pages/TelaEditarAtividade.jsx
+++ espectra-front/src/pages/TelaEditarAtividade.jsx
@@ -3,23 +3,81 @@ import Button from "../components/Button";
 import interrogacao from "../assets/general_photos/interrogacao.svg";
 import OutlinedTitle from "../components/OutlinedTitle";
 import logotipoAzul from "../assets/logotipos/logotipo-azul.svg";
-
+import ModalMeses from "../pages/ModalValorMeses";
+import { useState } from "react";
 import { useEffect } from "react";
+import OptionPaneMeses from "../components/OptionPaneMeses";
+import api from "../services/api";
 
 function telaEditarAtividade() {
     const navigate = useNavigate();
 
+    const [idAtividade, setIdAtividade] = useState(null)
+
     useEffect(() => {
         const id = Number(localStorage.getItem("id_atividade"))
+        
+        setIdAtividade(id)
 
-        console.log(id)
     }, [])
+
+    const idUsuario = 1 //localStorage.getItem('id_ususario')
+
+    const token = localStorage.getItem('token')
+
+    const [valorMeses, setValorMeses] = useState(1)
+    const [erroComportamento, setErroComportamento] = useState("")
+    const [mensagemSucesso, setMensagemSucesso] = useState("")
+    const [comportamento, setComportamento] = useState("")
+
+    async function editarAtividade() {
+        try {
+            const response = await api.put(
+                `v1/espectra/atividade/personalizada/${idAtividade}`, 
+                {
+                    id_usuario: idUsuario,
+                    comportamento: comportamento,
+                    valor_meses: Number(valorMeses)
+                },
+                {
+                    headers: {
+                        'x-access-token': token
+                    }
+                }
+            )
+
+            
+            const data = response.data
+
+            if(data.status_code == 200)
+                setMensagemSucesso("Atividade atualizada com sucesso!")
+
+                setTimeout(() => {
+                    navigate("/atividades")
+                }, 2000)
+               
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function salvarEdicao() {
+        if(comportamento.trim() === ""){
+            setErroComportamento("O comportamento é obrigatório")
+            return
+        }
+
+        setComportamento("")
+        editarAtividade()
+    } 
+
 
     return (
     
 
 
-        <div className="w-full lg:min-h-screen lg:bg-[var(--light-blue)] lg:flex lg:flex-col lg:items-center lg:justify-center ">
+        <div className="w-full lg:min-h-screen lg:bg-[var(--light-blue)] lg:flex lg:flex-col lg:items-center lg:justify-center">
 
             <img 
                 src={logotipoAzul}
@@ -27,7 +85,7 @@ function telaEditarAtividade() {
                 className="hidden lg:block "
             />
 
-            <div className="w-full h-screen px-8 flex flex-col lg:w-[60%] lg:bg-white lg:rounded-xl lg:h-[40%] lg:p-10 lg:gap-10 lg:shadow-[0_0_50px_rgba(0,0,0,0.25)]">
+            <div className="w-full h-screen px-8 flex flex-col lg:w-[60%] lg:bg-white lg:rounded-xl lg:h-[40%] lg:p-10 lg:gap-15 lg:shadow-[0_0_50px_rgba(0,0,0,0.25)]">
                 
                 <div className="flex justify-center w-full">
                     <OutlinedTitle>
@@ -38,52 +96,77 @@ function telaEditarAtividade() {
 
                 <div className="flex flex-col gap-25 mt-12 lg:mt-0">
 
+                  
                     <div className="flex flex-col w-full gap-3">
                         <p className="instrument-sans font-semibold text-lg md:text-2xl">Escreva a proposta de habilidade a ser desenvolvida:</p>
                         <input 
-                            type="text" 
-                            className="
-                                shadow-[0_0_50px_rgba(0,0,0,0.25)] w-full h-16 rounded-lg bg-[#C9C9C9] opacity-30
-                                md:w-[90%] md:h-40 md:self-center md:mr-8 
+                                    type="text" 
+                                    className="
+                                        shadow-[0_0_50px_rgba(0,0,0,0.25)]
+                                        w-full
+                                        h-16
+                                        rounded-lg
+                                        bg-[#C9C9C9]
+
+                                        px-4
+                                        text-black
+                                        placeholder:text-gray-500
+
+                                        md:w-[90%]
+                                        md:h-30
+                                        md:self-center
+                                        md:mr-8
+                                        md:text-xl
                                 "
-                                
-                            />
-                    </div>
+                                placeholder="Digite o comportamento..."
+                                onChange={(e) => {
+                                    setComportamento(e.target.value)
 
-                    <div className="flex flex-col gap-3">
+                                    if(e.target.value.trim() !== ""){
+                                        setErroComportamento("")
+                                    }
+                                }}
+                        />
 
-                        <p className="
-                            instrument-sans font-semibold text-lg md:text-2xl
-                            "
-                        >
-                            Nível de desenvolvimento em meses quando a atividade for concluida:
-                        </p>
+                            {erroComportamento && (
+                                <p className="text-red-500 text-sm md:self-center md:w-[90%]">
+                                    {erroComportamento}
+                                </p>
+                            )}
 
-                        <div className="flex w-full align-middle gap-1">
-                            <select 
-                                name="" 
-                                id="" 
-                                className="
-                                    shadow-[0_0_20px_rgba(0,0,0,0.25)] px-1 w-25 rounded-lg h-7 inclusive-sans text-sm 
-                                    md:text-2xl md:w-40 md:h-12 
-                                    "
-                                >
-                                <option value="">0 Meses</option>
-                                <option value="">1 Mês</option>
-                            </select>
-                                <img src={interrogacao} alt="sinal de interrogação" className="size-7 md:size-9" />
-                        </div>
-                    
+                            <OptionPaneMeses
+                                onChange={(valor) => {
+                                    setValorMeses(valor)
+                                }}
+                        />
+
                     </div>
 
                 </div>
 
+                {mensagemSucesso && (
+                            <div className="
+                                bg-green-500
+                                text-white
+                                p-3
+                                rounded-lg
+                                text-center
+                                font-semibold
+                                mt-3
+                                md:mt-5
+                                lg:
+                            ">
+                                {mensagemSucesso}
+                            </div>
+                        )}
 
-                <div className="flex flex-col mt-25 justify-items-center-safe w-full gap-3 md:gap-5 md:mt-40 lg:mt-0 lg:flex-row-reverse lg:w-full lg:justify-center">
+
+                <div className="flex flex-col mt-25 justify-items-center-safe w-full gap-3 md:gap-5 md:mt-10 lg:mt-0 lg:flex-row-reverse lg:w-full lg:justify-center">
                     <button className="
                         bg-[var(--bg-secondary-color)] w-70 rounded-md instrument-sans self-center text-white p-1
                         md:w-80 md:h-14 md:text-xl md:font-semibold md:rounded-2xl
                         "
+                        onClick={salvarEdicao}
                     >
                         Salvar alterações
                     </button>
@@ -92,6 +175,7 @@ function telaEditarAtividade() {
                         w-70 self-center text-[var(--bg-secondary-color)] shadow-[0_0_20px_rgba(0,0,0,0.25)] rounded-md p-1
                         md:w-80 md:h-14 md:text-xl md:font-semibold md:rounded-2xl
                         "
+                        onClick={() => {navigate('/atividades')}}
                         >
                             Cancelar
                     </button>
