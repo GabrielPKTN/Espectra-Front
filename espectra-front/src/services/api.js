6225bd35a8aca2d988ad01349e71791fc85b7a71
GabrielPKTN <gabriellacerda20059@outlook.com>
2026-06-13 17:48:18 -0300

    deploy: Muda endereço da API para a AZURE

--- espectra-front/src/services/api.js
+++ espectra-front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Substitua pela URL da sua API local ou em produção
-  baseURL: 'https://espectra-back.onrender.com', 
+  baseURL: 'https://minha-api-unica.azurewebsites.net', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
